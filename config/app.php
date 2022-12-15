1713431b4a954630773474f3315cfe617759da48
Petr Stastny <petrstastny09@gmail.com>
2022-12-15 10:58:36 +0100

    update welcame page and app config

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'WineApp'),
 
     /*
     |--------------------------------------------------------------------------
@@ -62,7 +62,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://wineapp.mrhappy.cz'),
 
     'asset_url' => env('ASSET_URL', null),
 
